7f1a2c5c46228e4c147c2d8f72497d06e25f0a36
zeus-space1 <officialelonmuskspacexoffice@gmail.com>
2025-05-07 04:15:14 +0100

    Create core.js

---
+++ core.js
@@ -0,0 +1,11 @@
+// ChocolateCore's magical brain
+console.log(`%c 
+❤🖤 CHOCOLATECORE ACTIVATED 🖤❤
+Type 'help' to see commands!
+`, "color: #ff6ec7; font-size: 16px;");
+
+function help() {
+    console.log("%cHere's what I can do:", "color: #6a5acd");
+    console.log("- Type 'hello' to greet me");
+    console.log("- Type 'memory' to see our GitHub history");
+}
